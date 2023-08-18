4d792c6273ddc187ac05dbd830a7690306e0b3d6
Tionne Whelan <80913553+t-whelan@users.noreply.github.com>
2023-08-18 10:20:38 +0200

    images

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import './App.css';
 
 
   const products = [
-    { id: 1, name: 'Apple Watch', price: 529.99, image: 'product1.jpg', make: 'Series 5 se', },
-    { id: 2, name: 'Sony ZX33OBT', price: 39.99, image: 'product2.jpg', make: 'Light Grey', },
-    { id: 3, name: 'Iphone 11', price: 619.99, image: 'product1.jpg', make: 'Serious Black',  },
-    { id: 4, name: 'Iphone 11', price: 619.99, image: 'product2.jpg', make: 'Subway Blue',  },
-    { id: 5, name: 'Iphone 11', price: 619.99, image: 'product2.jpg', make: 'Product RED',  },
-    { id: 6, name: 'Iphone 11', price: 619.99, image: 'product1.jpg', make: 'Milky White',  },
-    { id: 7, name: 'Iphone 13', price: 619.99, image: 'product2.jpg', make: 'Product RED',  },
-    { id: 8, name: 'Iphone 14', price: 619.99, image: 'product2.jpg', make: 'Product RED',  },
+    { id: 1, name: 'Apple Watch', price: 529.99, image: 'Apple-watch.png', make: 'Series 5 se', },
+    { id: 2, name: 'Sony ZX33OBT', price: 39.99, image: 'headphones.png', make: 'Light Grey', },
+    { id: 3, name: 'Iphone 11', price: 619.99, image: 'iphone11black4.jpg', make: 'Serious Black',  },
+    { id: 4, name: 'Iphone 11', price: 619.99, image: 'iphone11green.jpg', make: 'Subway Blue',  },
+    { id: 5, name: 'Iphone 11', price: 619.99, image: 'iphone11red.jpg', make: 'Product RED',  },
+    { id: 6, name: 'Iphone 11', price: 619.99, image: 'iphone11white.jpg', make: 'Milky White',  },
+    { id: 7, name: 'Iphone 13', price: 619.99, image: 'iphone13red2.jpg', make: 'Product RED',  },
+    { id: 8, name: 'Iphone 14', price: 619.99, image: 'iphone13red2.jpg', make: 'Product RED',  },
   
   
 
